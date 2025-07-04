c162155f73e9fb4dc82aa32ea1e978141ce6b629
nallari <shruti.nallari@gmail.com>
2025-07-03 23:20:49 -0400

    user data

--- .vscode/rtk-kit-sai/features/users/userSlice.js
+++ .vscode/rtk-kit-sai/features/users/userSlice.js
@@ -1,33 +1,33 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from "axios"
 
-const initialState={
-    loading:false,
-    users:[],
-    error:""
+const initialState = {
+    loading: false,
+    users: [],
+    error: ""
 }
 
-const fetchUsers=createAsyncThunk('user/fetchUsers',()=>{
+const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
     axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    .then((res)=>res.data)
+        .then((res) => res.data)
 })
 
-const userSlice=createSlice({
-    name:'user',
-    initialState:initialState,
-    extraReducers:(builder)=>{
-        builder.addCase(fetchUsers.pending, (state)=>{
-            state.loading=true
+const userSlice = createSlice({
+    name: 'user',
+    initialState: initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.pending, (state) => {
+            state.loading = true
         })
-        builder.addCase(fetchUsers.fulfilled, (state, action)=>{
-            state.loading=false
-            state.users=action.payload
-            state.error=""
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.loading = false
+            state.users = action.payload
+            state.error = ""
         })
-        builder.addCase(fetchUsers.rejected, (state,action)=>{
-            state.loading=false
-            state.error=action.error.message
-            state.users=[]
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.error.message
+            state.users = []
         })
     }
 
