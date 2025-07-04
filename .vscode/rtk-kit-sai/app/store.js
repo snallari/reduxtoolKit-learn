6c6d496585cb6e00989879a271bbfcd6895a1109
nallari <shruti.nallari@gmail.com>
2025-07-03 23:18:08 -0400

    Redux toolkit

--- .vscode/rtk-kit-sai/app/store.js
+++ .vscode/rtk-kit-sai/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cakeReducer from "../features/cake/cakeslice.js";
 import iceCreamReducer from "../features/icecream/icecreamSlice.js"
+import userReducer from "../features/users/userSlice.js"
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
-        iceCream:iceCreamReducer
+        iceCream:iceCreamReducer,
+        user:userReducer
     }
 });
 
--- .vscode/rtk-kit-sai/index.js
+++ .vscode/rtk-kit-sai/index.js
@@ -1,6 +1,7 @@
 import store from "./app/store.js";
 import { actions as cakeActions } from "./features/cake/cakeslice.js";
 import { actions as iceCreamActions } from "./features/icecream/icecreamSlice.js";
+import { fetchUsers } from "./features/users/userSlice.js";
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
@@ -10,4 +11,5 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(2));
 store.dispatch(iceCreamActions.ordered());
 store.dispatch(iceCreamActions.restocked(10));
+store.dispatch(fetchUsers())
 unsubscribe()
